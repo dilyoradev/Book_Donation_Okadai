cbbb5c83a5870f5fdeeb071a3b26b31a84c5dfc0
Dilyorakhon <128347223+dilyoradev@users.noreply.github.com>
2025-09-06 16:05:42 +0900

    Adding login model to model.py

--- models.py
+++ models.py
@@ -14,4 +14,11 @@ class Signup(db.Model):
 
     def __repr__(self):
         return f"<User {self.email}>"
-    
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def _repr_(self):
+        return f"<User {self.email}>"
