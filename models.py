4223c9e29e173c05ed477ed47f8538bcaee4b099
Dilyorakhon <128347223+dilyoradev@users.noreply.github.com>
2025-11-02 17:48:56 +0900

    Instance is created for BookRequest

--- models.py
+++ models.py
@@ -34,7 +34,7 @@ class BookRequest(db.Model):
     book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
     requester_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     status = db.Column(db.String(20), default="pending") #pending #accepted #complete
-    timestamp = db.Column(db.TimeDate, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 
--- routes/books.py
+++ routes/books.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
+
 
 books_bp = Blueprint("books", __name__)
 
-@books_bp.route("/book_details")
+@books_bp.route("/book_details/requests/<int:book_id>", method=["POST"])
+@login_required
 def book_details():
     return render_template("book-details.html")
